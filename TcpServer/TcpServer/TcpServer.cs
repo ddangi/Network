52f1ebf5d00f9e26b3e2e9d6e083276bf96160be
ddangi <nicengb@gmail.com>
2017-10-26 23:34:01 +0900

    submit new TcpServer Project that using ServerBase project

---
+++ TcpServer/TcpServer/TcpServer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpServer
+{
+    public partial class TcpServer : ServiceBase
+    {
+        public TcpServer()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ TcpServer/TcpServer/TcpServiceInstaller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+
+namespace TcpServer
+{
+    [RunInstaller(true)]
+    public partial class TcpServiceInstaller : System.Configuration.Install.Installer
+    {
+        private ServiceInstaller serviceInstaller;
+
+        public TcpServiceInstaller()
+        {
+            InitializeComponent();
+
+            serviceInstaller = new ServiceInstaller();
+
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+            serviceInstaller.ServiceName = " TcpServer";
+            serviceInstaller.Description = "Tcp Server Service";
+
+            var servicesDependedOn = new List<string> { "tcpip" };
+
+            serviceInstaller.ServicesDependedOn = servicesDependedOn.ToArray();
+
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
