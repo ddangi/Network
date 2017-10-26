ade67d3d4db0791da3c3f3c6913de8f31c22d7e3
ddangi <nicengb@gmail.com>
2017-10-27 00:17:24 +0900

    I'm writing entry point

--- TcpServer/TcpServer/Program.cs
+++ TcpServer/TcpServer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration.Install;
 using System.Linq;
+using System.Reflection;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +11,99 @@ namespace TcpServer
 {
     static class Program
     {
+        private static readonly string _exePath = Assembly.GetExecutingAssembly().Location;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main()
+        static void Main(string[] args)
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
+            if (0 < args.Length)
             {
-                new TcpServer()
-            };
-            ServiceBase.Run(ServicesToRun);
+                string exeArg = args[1];
+
+                switch (exeArg.ToLower())
+                {
+                    case ("-i"):
+                    case ("-install"):
+                        Install();
+                        break;
+
+                    case ("-u"):
+                    case ("-uninstall"):
+                        Uninstall();
+                        break;
+
+                    case ("-d"):
+                    case ("-debug"):
+                        RunAsConsole();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid argument!");
+                        break;
+                }
+            }
+            else
+            {
+                RunAsService();
+            }
+        }
+
+        private static void SetConsoleColor(ConsoleColor color)
+        {
+            if (setConsoleColor)
+                Console.ForegroundColor = color;
+        }
+
+        private static void Install()
+        {
+            try
+            {
+                ManagedInstallerClass.InstallHelper(new string[] { _exePath });
+            }
+            catch
+            {
+                Console.WriteLine("failed to install Service!");
+            }
+        }
+
+        private static void Uninstall()
+        {
+            try
+            {
+                ManagedInstallerClass.InstallHelper(new string[] { "/u", _exePath });
+            }
+            catch
+            {
+                Console.WriteLine("failed to install Service!");
+            }
+        }
+
+        private static void RunAsService()
+        {
+            ServiceBase[] servicesToRun;
+
+            servicesToRun = new ServiceBase[] { new TcpServer() };
+
+            ServiceBase.Run(servicesToRun);
+        }
+
+        private static bool setConsoleColor;
+
+        private static void SetConsoleColor(/*LogLevel*/)
+        {
+            SetConsoleColor(ConsoleColor.Red);
+            SetConsoleColor(ConsoleColor.Green);
+        }
+
+        static void RunAsConsole()
+        {
+            Console.WriteLine("-------------------------------------------------------------------");
+            Console.WriteLine("Try to start TcpServer on console mode!");
+            Console.WriteLine("-------------------------------------------------------------------");
+
+
+            Console.WriteLine("-------------------------------------------------------------------");
         }
     }
 }
