66933b3dd7340f5d03c5cac9a5f23cdfe83a7d04
ddangi <nicengb@gmail.com>
2017-10-26 23:20:13 +0900

    Set new ServerBase project

---
+++ ServerBase/ServerBase/SocketAsyncEventArgsPool.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerBase
+{
+    public class SocketAsyncEventArgsPool
+    {
+        //just for assigning an ID so we can watch our objects while testing.
+        private Int32 nextTokenId = 0;
+
+        // Pool of reusable SocketAsyncEventArgs objects.
+        Stack<SocketAsyncEventArgs> pool;
+
+        // initializes the object pool to the specified size.
+        // "capacity" = Maximum number of SocketAsyncEventArgs objects
+        internal SocketAsyncEventArgsPool(Int32 capacity)
+        {
+            this.pool = new Stack<SocketAsyncEventArgs>(capacity);
+        }
+
+        // The number of SocketAsyncEventArgs instances in the pool.
+        internal Int32 Count
+        {
+            get { return this.pool.Count; }
+        }
+
+        // Removes a SocketAsyncEventArgs instance from the pool.
+        // returns SocketAsyncEventArgs removed from the pool.
+        internal SocketAsyncEventArgs Pop()
+        {
+            lock (this.pool)
+            {
+                return this.pool.Pop();
+            }
+        }
+
+        // Add a SocketAsyncEventArg instance to the pool.
+        // "item" = SocketAsyncEventArgs instance to add to the pool.
+        internal bool Push(SocketAsyncEventArgs item)
+        {
+            if (item == null)
+            {
+                //throw new ArgumentNullException("Items added to a SocketAsyncEventArgsPool cannot be null");
+                return false;
+            }
+
+            lock (this.pool)
+            {
+                this.pool.Push(item);
+            }
+            return true;
+        }
+    }
+}
