bfcbff3cd308506fe799caf6e65ef18f415e09f8
BH.Kim <32698008+ddangi@users.noreply.github.com>
2017-10-30 10:56:08 +0900

    Update PacketBase.cs

--- ServerBase/ServerBase/Network/PacketBase.cs
+++ ServerBase/ServerBase/Network/PacketBase.cs
@@ -12,6 +12,20 @@ namespace ServerBase
     {
         public short length;    // 2
         public short cmd;       // 2
+        
+        public byte[] GetBytes()
+        {
+            byte[] buffer = new byte[Constants.HEADER_SIZE];
+            int offset = 0;
+            
+            byte[] ar = BitConverter.GetBytes(length);
+            Buffer.BlockCopy(ar, 0, buffer, offset, sizeof(short));
+            offset += sizeof(short);
+            
+            byte[] cmdBy = BitConverter.GetBytes(length);
+            Buffer.BlockCopy(cmdBy, 0, buffer, offset, sizeof(short));
+            offset += sizeof(short);
+        }
     }
 
     abstract public class PacketBase
